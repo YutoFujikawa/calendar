5455256fe1916a6bc614cf76ac1f89b1e0cb88dc
fjkw0409 <fjkw0409@gmail.com>
2025-01-20 02:23:57 +0900

    add_textStyles

---
+++ lib/src/styles/text_styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TextStyle sundayStyle = TextStyle(color: Colors.red);
+  static const TextStyle saturdayStyle = TextStyle(color: Colors.blue);
+}
